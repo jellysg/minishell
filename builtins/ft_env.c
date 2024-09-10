cf53e9adbd70943c15e5615c1e53b6f52ce93245
wchow <nexiliux@gmail.com>
2024-09-10 18:17:23 +0800

    test

--- builtins/ft_env.c
+++ builtins/ft_env.c
@@ -6,7 +6,21 @@
 /*   By: wchow <wchow@42mail.sutd.edu.sg>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:11:21 by wchow             #+#    #+#             */
-/*   Updated: 2024/09/10 18:11:23 by wchow            ###   ########.fr       */
+/*   Updated: 2024/09/10 18:16:38 by wchow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+ahjkdslfhljakdhsfkadjhf
+a;sdkfhakljdhfaklsjdhfjkadhsf
+a;sdkjfhajkldhsfkjalhdsfkjahdsf
+asdfkljahsdlfkjhasdklfhadfjk
+ahjkdslfhljakdhsfkadjhf
+a;sdkfhakljdhfaklsjdhfjkadhsf
+a;sdkjfhajkldhsfkjalhdsfkjahdsf
+asdfkljahsdlfkjhasdklfhadfjkahjkdslfhljakdhsfkadjhf
+a;sdkfhakljdhfaklsjdhfjkadhsf
+a;sdkjfhajkldhsfkjalhdsfkjahdsf
+asdfkljahsdlfkjhasdklfhadfjkahjkdslfhljakdhsfkadjhf
+a;sdkfhakljdhfaklsjdhfjkadhsf
+a;sdkjfhajkldhsfkjalhdsfkjahdsf
+asdfkljahsdlfkjhasdklfhadfjk
