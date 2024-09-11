99b172b582de9f0f1fe3d6f66766a1152aa1cad6
wchow <nexiliux@gmail.com>
2024-09-11 20:32:50 +0800

    finalised, can use /bin/pwd with random argument to differentiate between builtin pwd and /bin/pwd.

--- src/ft_pwd.c
+++ src/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: wchow <wchow@42mail.sutd.edu.sg>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 20:03:11 by wchow             #+#    #+#             */
-/*   Updated: 2024/09/11 20:27:12 by wchow            ###   ########.fr       */
+/*   Updated: 2024/09/11 20:31:54 by wchow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,15 @@
 
 int	ft_pwd(t_data *data)
 {
-	printf("{builtin pwd}\n");
-	if (data->args && data->args[1])
-		printf("Invalid. Too many arguments\n");
-	else
+	if (data->cwd)
+		free (data->cwd);
+	data->cwd = malloc(PATH_MAX);
+	if (!getcwd(data->cwd, PATH_MAX))
 	{
-		if (data->cwd)
-			free (data->cwd);
-		data->cwd = malloc(PATH_MAX);
-		if (!getcwd(data->cwd, PATH_MAX))
-		{
-			perror("getcwd failed.");
-			free(data->cwd);
-			return (FAILURE);
-		}
-		printf("%s\n", data->cwd);
+		perror("getcwd failed.");
+		free(data->cwd);
+		return (FAILURE);
 	}
+	printf("%s\n", data->cwd);
 	return (SUCCESS);
 }
